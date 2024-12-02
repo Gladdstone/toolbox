758a82595848fc81dbb49db1842b89e4a47ec23d
Joseph Farrell <disastrousgamemaster@gmail.com>
2024-12-01 22:44:37 -0800

    add buildroot

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,22 +1,36 @@
-# building linux kernel on aarch64
+# building linux kernel as x86_64 on aarch64 (M1 Mac)
 # not for running so much as it is for notes
 apt-get update && apt-get upgrade
 
-apt-get install build-essential
-apt-get install gcc g++ gcc-doc
-apt-get install libncurses-dev
-apt-get install gcc-aarch64-linux-gnu
-apt-get install flex
-apt-get install bison
-apt install libssl-dev
+apt-get install build-essential libncurses5-dev gcc-x86-64-linux-gnu g++-x86-64-linux-gnu flex bison libssl-dev libelf-dev
 
 # configure linux
-# make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- defconfig
-# make ARCH=arm64 menuconfig
-# make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- all
+# make ARCH=x86_64 CROSS_COMPILE=x86_64-linux-gnu- defconfig
+# enable debugging in kernel hacking
+make ARCH=x86_64 menuconfig
+make ARCH=x86_64 CROSS_COMPILE=x86_64-linux-gnu- -j{cores}
 
-# configure busybox filesystem
-# currently, Ubuntu 24 is not supported by Busybox: https://bugs.busybox.net/show_bug.cgi?id=15931
+apt-get install qemu qemu-system
+
+qemu-system-x86_64 -kernel arch/x86/boot/bzImage -hda /dev/zero -append "root=/dev/zero console=ttyS0" -serial stdio -display none
+
+# Buildroot-based filesystem
+wget http://buildroot.org/downloads/buildroot-2024.08.tar.gz
+git clone https://gitlab.com/buildroot.org/buildroot.git
+
+# set target architecture to x86_64
+# filesystem images -> ext4
+make menuconfig
+make ARCH=x86_64 -j2
+
+# running the kernel
+# user - root; password -
+qemu-system-x86_64 -s -kernel arch/x86/boot/bzImage -boot c -m 2049M -hda <path to buildroot>/output/images/rootfs.ext4 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" -serial stdio -display none
+# running the kernel w/ gdb
+qemu-system-x86_64 -s -S  -kernel arch/x86/boot/bzImage -boot c -m 2049M -hda ~/Downloads/buildroot-2021.02.8/output/images/rootfs.ext4 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" -serial stdio -display none
+
+# Busybox-based filesystem
+# Note: currently Ubuntu 24 is not supported by Busybox: https://bugs.busybox.net/show_bug.cgi?id=15931
 # Recommend Ubuntu 22 or Debian
 wget https://busybox.net/downloads/busybox-1.36.1.tar.bz2
 tar -xvf busybox-1.36.1.tar.bz2
@@ -32,7 +46,7 @@ cd ../
 gzip -c rootfs.img > rootfs.img.gz
 
 # run kernel
-qemu-system-arm -M virt -m 128M -nographic -kernel Image -initrd rootfs.img.gz -append "root=/dev/ram rdinit=/bin/sh"
+qemu-system-aarch64 -M virt -m 128M -nographic -kernel Image -initrd rootfs.img.gz -append "root=/dev/ram rdinit=/bin/sh"
 
 # configure sys and dev directories
 cd _install
@@ -46,5 +60,5 @@ mount -t sysfs none /sys
 /sbin/mdev -s
 # make executable with chmod
 
-qemu-system-arm -M virt -m 128M -nographic -kernel Image -initrd rootfs.img.gz -append "root=/dev/ram rdinit=/sbin/init"
+qemu-system-aarch64 -M virt -m 128M -nographic -kernel Image -initrd rootfs.img.gz -append "root=/dev/ram rdinit=/sbin/init"
 
