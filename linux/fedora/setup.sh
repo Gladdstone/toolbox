4be21a16e6efbce323982c35d9fcd277531bd655
Joseph Farrell <joe@joefarrell.dev>
2026-02-02 20:42:03 -0800

    update fedora setup and cleanup zshrc

--- linux/fedora/setup.sh
+++ linux/fedora/setup.sh
@@ -34,7 +34,21 @@ dnf install \
     fortune \
     sl
 
+dnf copr enable lihaohong/yazi
+dnf install yazi
+
 # set screen lock
 kwriteconfig6 --file kscreenlockerrc --group Daemon --key Timeout 600
 kwriteconfig6 --file kscreenlockerrc --group Daemon --key Autolock true
+
+# eww setup
+cd ~/code
+git clone https://github.com/elkowar/eww
+cd ./eww
+cargo build --release --no-default-features --features=wayland
+cd ~/
+mkdir -p ./.config/eww/
+touch ./.config/eww/eww.yuck ./.config/eww/eww.scss
+
+cargo install viu
 
